3f718ac02a3e072d103c3bd1f881f24494e69046
JJ Merelo <jjmerelo@gmail.com>
2024-11-05 07:28:52 +0100

    #23 adaptations to data type

--- script/run-pinpoint-command.pl
+++ script/run-pinpoint-command.pl
@@ -17,30 +17,33 @@ my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)
 my $suffix = "$day-$mon-$hh-$mm-$ss";
 
 open my $fh, ">", "$data_dir/$preffix-$function-$suffix.csv";
-say $fh "Platform,size,PKG,seconds";
+say $fh "work,type,size,PKG,seconds";
 
-my $work = $function ? "-f $function":"";
-for my $l ( qw(128 256 512) ) {
-  my $total_seconds;
-  my $successful = 0;
-  my @results;
-  do {
-    my $command = "./fp -i $l $work";
-    say $command;
-    my $output = `pinpoint -- $command 2>&1`;
-    say $output;
-    my ( $gpu, $pkg, $seconds ) = process_pinpoint_output $output;
-    if ($gpu != 0 ) {
-      $successful++;
-      $total_seconds += $seconds;
-      say "$preffix, $function, $l,$pkg, $seconds";
-      push @results, [$pkg,$seconds];
+my $work = $function ne "generation" ? "-f $function":"";
+
+for my $t ( qw(f d ) {
+  for my $l ( qw(128 256 512) ) {
+    my $total_seconds;
+    my $successful = 0;
+    my @results;
+    do {
+      my $command = "./fp -i $l -t $t $work";
+      say $command;
+      my $output = `pinpoint -- $command 2>&1`;
+      say $output;
+      my ( $gpu, $pkg, $seconds ) = process_pinpoint_output $output;
+      if ($gpu != 0 ) {
+        $successful++;
+        $total_seconds += $seconds;
+        say "$preffix, $function, $t,  $l,$pkg, $seconds";
+        push @results, [$pkg,$seconds];
+      }
+    } while ( $successful < $ITERATIONS );
+
+    foreach  my $row (@results) {
+      say join(", ", @$row);
+      say $fh "$function, $t, $l, ", join(", ", @$row);
     }
-  } while ( $successful < $ITERATIONS );
-
-  foreach  my $row (@results) {
-    say join(", ", @$row);
-    say $fh "$function, $l, ", join(", ", @$row);
   }
 }
 close $fh;
