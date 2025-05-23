639c97a6feb0b80c83714a385b5d44d7c96dff81
JJ Merelo <jjmerelo@gmail.com>
2025-05-23 21:04:07 +0200

    Plotting energy and watts

--- script/watts-order.R
+++ script/watts-order.R
@@ -19,12 +19,26 @@ ggplot(bbob.fixed, aes(x=order, y=PKG,shape=type)) +
   geom_point( aes(color=work)) + scale_color_brewer(palette="Set1") +
   labs(title="Watts consumed in every experiment evaluating 40K chromosomes with 128, 256, 512 dimensions, using float or double for every one of them. Please note the $y$ axes have different scales", y="PKG", x="Experiment") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  scale_y_continuous(limits = c(0, 25))
+  scale_y_continuous(limits = c(0, 5))
 
 bbob.variable <- read.csv("data/variable-evostar25-bbob-10-Nov-19-10-32.csv")
 bbob.variable$watts <- bbob.variable$PKG / bbob.variable$seconds
-# plot bbob.variable$watts by row order
 bbob.variable$size <- as.factor(bbob.variable$size)
+bbob.variable <- bbob.variable %>%
+  group_by(type, size, work) %>%
+  mutate(order = row_number()) %>%
+  ungroup()
+
+ggplot(bbob.variable, aes(x=order, y=PKG,shape=type)) +
+  geom_point( aes(color=work)) + scale_color_brewer(palette="Set1") +
+  labs(title="PKG consumed depending on experiment order", y="PKG", x="Experiment") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(bbob.variable, aes(x=order, y=watts,shape=type)) +
+  geom_point( aes(color=work)) + scale_color_brewer(palette="Set1") +
+  labs(title="PKG consumed depending on experiment order", y="PKG", x="Experiment") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
 ggplot(bbob.variable[bbob.variable$work == "rastrigin",], aes(x=seq_along(watts), y=watts, shape=type,color=size)) +
   geom_point() +
   labs(title="Watts consumed in every experiment evaluating 40K chromosomes with 128, 256, 512 dimensions, using float or double for every one of them. Please note the $y$ axes have different scales", y="Watts", x="Experiment") +
